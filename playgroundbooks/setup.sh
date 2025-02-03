ef1fc4f774e53dedf97f93c8bc742392751c27ed
Mark Schmidt <mark.schmidt@hey.com>
2025-02-03 13:18:26 -0500

    qnd

--- playgroundbooks/setup.sh
+++ playgroundbooks/setup.sh
@@ -1,5 +1,7 @@
 (cd $CONSOLE_BOOK_PATH && zip -r text.playgroundbook.zip "Text Playground.playgroundbook")
 (cd $CONSOLE_BOOK_PATH && zip -r turtle.playgroundbook.zip "Turtle Playground.playgroundbook")
 
+cp $CONSOLE_BOOK_PATH/text.playgroundbook.zip playgroundbooks/text/text.zip
+cp $CONSOLE_BOOK_PATH/turtle.playgroundbook.zip playgroundbooks/turtle/turtle.zip
 cp $CONSOLE_BOOK_PATH/text.playgroundbook.zip playgroundbooks/text/text.playgroundbook.zip
 cp $CONSOLE_BOOK_PATH/turtle.playgroundbook.zip playgroundbooks/turtle/turtle.playgroundbook.zip
